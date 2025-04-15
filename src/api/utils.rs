1a710cb65ca8d3cb2637ea21975efa78fceae751
Priceless-P <prisca.priceless11@gmail.com>
2025-04-15 11:54:46 +0100

    Normalize cpu_usage_%
    - Modified the get_cpu_and_memory_usage function to normalize CPU usage to a 0-100% range by dividing the cpu_usage value by the number of logical CPUs (system.cpus().len()), as recommended by sysinfo documentation.  This is necessary because sysinfo's cpu_usage aggregates usage across all CPU cores, potentially exceeding 100% on multi-core systems. Dividing it by number of cpus ensures it reflects usage relative to a single core.

--- src/api/utils.rs
+++ src/api/utils.rs
@@ -1,4 +1,3 @@
-use std::time::Duration;
 use sysinfo::{Pid, ProcessRefreshKind, ProcessesToUpdate, System};
 
 pub(super) async fn get_cpu_and_memory_usage() -> (f32, u64) {
@@ -12,7 +11,7 @@ pub(super) async fn get_cpu_and_memory_usage() -> (f32, u64) {
     );
 
     // Wait for a measurable interval
-    tokio::time::sleep(Duration::from_millis(100)).await;
+    tokio::time::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL).await;
 
     // Second refresh to get the difference
     system.refresh_processes_specifics(
@@ -24,8 +23,16 @@ pub(super) async fn get_cpu_and_memory_usage() -> (f32, u64) {
     let pid = std::process::id();
     if let Some(process) = system.process(Pid::from_u32(pid)) {
         let cpu_usage = process.cpu_usage();
+        let cpu_nums = system.cpus().len() as f32; // get the number of cpu
+        
+        let normalized_cpu_usage = if cpu_nums > 0.0 {
+            cpu_usage / cpu_nums
+        } else {
+            0.0
+        };
+
         let memory = process.memory();
-        (cpu_usage, memory)
+        (normalized_cpu_usage, memory)
     } else {
         (0.0, 0)
     }
